d721d8e6ddca5e7e7b44d3a8f399845b9cf71e48
Vuli Russell <vulirussell@gmail.com>
2020-11-14 18:54:03 +0000

    styled header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import AddToDoItem from "./AddToDoItem";
 import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { signIn, signOut } from "../../services/userServices";
 
 import { connect } from "react-redux";
@@ -25,10 +27,16 @@ class Header extends Component {
         <div className={styles.headerButtons}>
           {this.props.user&&this.props.user.state ? 
             <>
-              <button onClick={() => this.handleOpenToDoItem()}>Add to do item</button>
-              <p>{this.props.user.state.displayName}</p>
-              <img src={this.props.user.state.photoURL} alt="profile"/>
-              <button onClick={signOut}>SignOut</button>
+              <div className={styles.faIcon} onClick={() => this.handleOpenToDoItem()}>
+                <FontAwesomeIcon icon={faPlus} />
+              </div>
+              <div className={styles.user}>
+                <p>{this.props.user.state.displayName}</p>
+                <img src={this.props.user.state.photoURL} alt="profile"/>
+                <div className={styles.faIcon} onClick={signOut}>
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </div>
+              </div>
             </>
             : 
             <button onClick={signIn}>Sign In</button> }
