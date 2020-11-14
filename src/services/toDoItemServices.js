a488e725cc9814446baa9c1df8c09a225b0e5c00
Vuli Russell <vulirussell@gmail.com>
2020-11-14 23:57:08 +0000

    added heroku api url

--- src/services/toDoItemServices.js
+++ src/services/toDoItemServices.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const url = "https://vuli-todo-list-api.herokuapp.com/"
+
 export const getToDoItems = async (userID) => {
     try{
-        return await axios.get(`http://localhost:8080/todo/get?user=${userID}`)
+        return await axios.get(`${url}todo/get?user=${userID}`)
     }catch (error){
         return error
     }
@@ -10,7 +12,7 @@ export const getToDoItems = async (userID) => {
 
 export const addToDoItem = async(document) => {
     try{
-        return await axios.post("http://localhost:8080/todo/post/",{...document}) 
+        return await axios.post(`${url}todo/post/`,{...document}) 
     } catch (error){
         return error
     }
@@ -18,7 +20,7 @@ export const addToDoItem = async(document) => {
 
 export const updateToDoItem = async(document) => {
     try{
-        return await axios.put("http://localhost:8080/todo/put", {...document})
+        return await axios.put(`${url}todo/put`, {...document})
     }catch(error){
         return error
     }
@@ -26,7 +28,7 @@ export const updateToDoItem = async(document) => {
 
 export const deleteToDoItem = async(_id) => {
     try{
-        return await axios.delete("http://localhost:8080/todo/delete/",{data: {_id: _id}})
+        return await axios.delete(`${url}todo/delete/`,{data: {_id: _id}})
     }catch(error){
         return error
     }
